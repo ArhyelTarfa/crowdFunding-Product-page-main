0db48221a13eda8d4b4f2723e0045221f4c8e694
Arhyel <arhyeltarfa2@gmail.com>
2023-02-04 21:56:15 +0100

    further improvment

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
       backgroundImage: {
        'desktop' : "url('../images/image-hero-desktop.jpg')",
         'mobile': "url('../images/image-hero-mobile.jpg')",
+      },
+      colors:{
+        'Moderatecyan': "hsl(176, 50%, 47%)",
+        'Darkcyan': "hsl(176, 72%, 28%)",
+        "Darkgray": 'hsl(0, 0%, 48%)'
+      },
+      fontFamily:{
+        'commissioner': ['Inter', 'sans-serif']
       }
     },
   },
